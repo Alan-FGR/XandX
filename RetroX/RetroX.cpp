988d07d4a0d4cbc041348d7e8a95ddba9135f853
Alan <alanqdev@gmail.com>
2019-02-22 00:40:09 -0300

    dd

--- RetroX/RetroX.cpp
+++ RetroX/RetroX.cpp
@@ -2,9 +2,30 @@
 //
 
 #include "pch.h"
-#include <iostream>
+#include <cstdio>
+
+#define SDL_MAIN_HANDLED
+#include "SDL.h"
+
+void fatal(const char* err)
+{
+    printf(err);
+    printf(SDL_GetError());
+    exit(-1);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) fatal("SDL couldn't initialize");
+    
+    auto win = SDL_CreateWindow("RetroX", 0,0,400,300,SDL_WINDOW_BORDERLESS);
+
+    if (win == nullptr) fatal("Couldn't create window");
+
+
+
+    printf("SDL meh");
+
+    return 0;
 }
