972001c75205241fddb020c2aee9080b02a9ba45
Alan <alanqdev@gmail.com>
2019-02-23 21:58:30 -0300

    lotsa stuffs

--- Hooking/dllmain.cpp
+++ Hooking/dllmain.cpp
@@ -6,27 +6,125 @@
 #include "SDL.h"
 #include <cstdio>
 
-__declspec(dllexport) void magic() {}
+__declspec(dllexport) void magic() {} // :notes: I never did beliee-ee-ee-eeve in the ways of magic... :trollface:
 
-static int(*origSDL_RenderClear)(SDL_Renderer* renderer) = SDL_RenderClear;
-
-static BOOL(WINAPI *origGetClientRect)(_In_ HWND hWnd, _Out_ LPRECT lpRect) = GetClientRect;
-
-BOOL iGetClientRect(_In_ HWND hWnd, _Out_ LPRECT lpRect)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    auto r = origGetClientRect(hWnd, lpRect);
-    lpRect->right = lpRect->left+400;
-    lpRect->bottom = lpRect->top+300;
-    return r;
+    switch (msg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
 }
 
-int iClear(SDL_Renderer* renderer)
+static HWND textArea;
+void createLogger(HINSTANCE hInstance)
 {
-    printf("sux\n");
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    auto r = origSDL_RenderClear(renderer);
-    //SDL_RenderClear(renderer);
+
+    auto className = L"Hook Logger";
+    
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = className;
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, L"Window Registration Failed!", L"Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        className,
+        className,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 400, 400,
+        NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, L"Window Creation Failed!", L"Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return;
+    }
+
+    textArea = CreateWindowEx(WS_EX_CLIENTEDGE, L"edit", L"LOG START\n",
+        WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOHSCROLL, 0, 0, 400, 380, hwnd, NULL, NULL, NULL);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+}
+
+void log(const char* msg, bool pause = false)
+{
+    int index = GetWindowTextLength(textArea);
+    SetFocus(textArea);
+    SendMessageA(textArea, EM_SETSEL, (WPARAM)index, (LPARAM)index);
+    SendMessageA(textArea, EM_REPLACESEL, 0, (LPARAM)msg);
+}
+
+
+HWND(WINAPI *origCreateWindowExA)(_In_ DWORD dwExStyle, _In_opt_ LPCSTR lpClassName, _In_opt_ LPCSTR lpWindowName, _In_ DWORD dwStyle, _In_ int X, _In_ int Y, _In_ int nWidth, _In_ int nHeight, _In_opt_ HWND hWndParent, _In_opt_ HMENU hMenu, _In_opt_ HINSTANCE hInstance, _In_opt_ LPVOID lpParam) = CreateWindowExA;
+HWND WINAPI itcpCreateWindowExA(_In_ DWORD dwExStyle, _In_opt_ LPCSTR lpClassName, _In_opt_ LPCSTR lpWindowName, _In_ DWORD dwStyle, _In_ int X, _In_ int Y, _In_ int nWidth, _In_ int nHeight, _In_opt_ HWND hWndParent, _In_opt_ HMENU hMenu, _In_opt_ HINSTANCE hInstance, _In_opt_ LPVOID lpParam) {
+    auto r = origCreateWindowExA(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
     return r;
+}
+void hookCreateWindowExA() { DetourAttach(&(PVOID&)origCreateWindowExA, itcpCreateWindowExA); }
+void detachCreateWindowExA() { DetourDetach(&(PVOID&)origCreateWindowExA, itcpCreateWindowExA); }
+
+HWND(WINAPI *origCreateWindowExW)(_In_ DWORD dwExStyle, _In_opt_ LPCWSTR lpClassName, _In_opt_ LPCWSTR lpWindowName, _In_ DWORD dwStyle, _In_ int X, _In_ int Y, _In_ int nWidth, _In_ int nHeight, _In_opt_ HWND hWndParent, _In_opt_ HMENU hMenu, _In_opt_ HINSTANCE hInstance, _In_opt_ LPVOID lpParam) = CreateWindowExW;
+HWND WINAPI itcpCreateWindowExW(_In_ DWORD dwExStyle, _In_opt_ LPCWSTR lpClassName, _In_opt_ LPCWSTR lpWindowName, _In_ DWORD dwStyle, _In_ int X, _In_ int Y, _In_ int nWidth, _In_ int nHeight, _In_opt_ HWND hWndParent, _In_opt_ HMENU hMenu, _In_opt_ HINSTANCE hInstance, _In_opt_ LPVOID lpParam) {
+    auto r = origCreateWindowExW(dwExStyle, lpClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
+    return  r;
+}
+void hookCreateWindowExW() { DetourAttach(&(PVOID&)origCreateWindowExW, itcpCreateWindowExW); }
+void detachCreateWindowExW() { DetourDetach(&(PVOID&)origCreateWindowExW, itcpCreateWindowExW); }
+
+BOOL(WINAPI *origGetClientRect)(_In_ HWND hWnd, _Out_ LPRECT lpRect) = GetClientRect;
+BOOL WINAPI itcpGetClientRect(_In_ HWND hWnd, _Out_ LPRECT lpRect) {
+    auto r = origGetClientRect(hWnd, lpRect);
+    lpRect->right = lpRect->left + 400;
+    lpRect->bottom = lpRect->top + 300;
+    return r;
+}
+void hookGetClientRect() { DetourAttach(&(PVOID&)origGetClientRect, itcpGetClientRect); }
+void detachGetClientRect() { DetourDetach(&(PVOID&)origGetClientRect, itcpGetClientRect); }
+
+BOOL(WINAPI *origGetWindowRect)(_In_ HWND hWnd, _Out_ LPRECT lpRect) = GetWindowRect;
+BOOL WINAPI itcpGetWindowRect(_In_ HWND hWnd, _Out_ LPRECT lpRect) {
+    auto r = origGetWindowRect(hWnd, lpRect);
+    return r;
+}
+void hookGetWindowRect() { DetourAttach(&(PVOID&)origGetWindowRect, itcpGetWindowRect); }
+void detachGetWindowRect() { DetourDetach(&(PVOID&)origGetWindowRect, itcpGetWindowRect); }
+
+
+
+
+
+
+
+void browse()
+{
+
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReason, LPVOID lpReserved)
@@ -37,17 +135,28 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReason, LPVOID lpReserved)
 
     if (dwReason == DLL_PROCESS_ATTACH) {
         printf("attaching");
+
+        createLogger(hModule);
+
         DetourRestoreAfterWith();
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        //DetourAttach(&(PVOID&)origSDL_RenderClear, iClear);
-        DetourAttach(&(PVOID&)origGetClientRect, iGetClientRect);
+
+        //attach stuff
+        hookGetClientRect();
+        hookCreateWindowExA();
+        hookCreateWindowExW();
+        hookGetWindowRect();
+
         DetourTransactionCommit();
     }
     else if (dwReason == DLL_PROCESS_DETACH) {
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        //DetourDetach(&(PVOID&)origSDL_RenderClear, iClear);
+
+        //detach stuff
+        detachGetClientRect();
+
         DetourTransactionCommit();
     }
     return TRUE;
---
+++ extractor.py
@@ -0,0 +1,70 @@
+import random, tkinter
+
+def convert(txt):
+    
+    txt = txt.replace("\r\n", ' ');
+    txt = txt.replace("\n", ' ');
+    for i in range(16): # :trollface:
+        txt = txt.replace("  ", ' ');
+
+    sigs = [x.strip() for x in txt.split(";")]
+
+    out = []
+
+    for sig in sigs:
+        if not sig:
+            continue
+        try:
+            spcsplit = sig.split(" ");
+
+            ftype = spcsplit[0]
+
+            try:
+                if spcsplit[1] == "*" or spcsplit[2][0] == "*":
+                    ftype += "*"
+            except:pass
+
+            #sux
+            api = ''
+            for kw in sig.split("(")[0].split(" "):
+                if ('API' in kw):
+                    api = kw
+                    break;
+
+            fname = sig.split("(")[0].split(" ")[-1]
+
+            fargs = sig.split("(")[1].split(")")[0].split(",")
+
+            fptw = ftype+" ("+api+" *orig"+fname+")("+",".join(fargs)+") = "+fname+";"
+
+            fisg = ' '.join(sig.split("(")[0].split(" ")[:-1])+" itcp"+fname+"("+",".join(fargs)+") {\n"+\
+                '    log("hooked itcp'+fname+' called");\n'+\
+                '    auto r = orig'+fname+"("+', '.join([p.split(' ')[-1] for p in fargs])+");\n    todo\n}"
+
+            fatt = "void hook"+fname+"(){DetourAttach(&(PVOID&)orig"+fname+", itcp"+fname+");}\n"
+            fdet = "void detach"+fname+"(){DetourDetach(&(PVOID&)orig"+fname+", itcp"+fname+");}\n"
+
+            out.append(fptw+"\n"+fisg+"\n"+fatt+fdet+"\n")
+
+        except:
+            out.append("ERROR")
+
+    return '\n'.join(out)+'\n'
+
+root = tkinter.Tk()
+root.geometry("674x512")
+root.resizable(False,False)
+
+intext = tkinter.Text(root, height=14)
+outtext = tkinter.Text(root, height=14, wrap=tkinter.NONE)
+
+def gui_convert():
+    outtext.delete('1.0', 'end') # wtf
+    outtext.insert('1.0',convert(intext.get('1.0','end'))) #what the actual hell?? :(
+
+tkinter.Label(root,text="input decls").pack()
+intext.pack()
+button = tkinter.Button(root, text="extract hooks", command=gui_convert).pack()
+outtext.pack()
+
+root.mainloop()
